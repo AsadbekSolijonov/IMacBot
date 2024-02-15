6258b151bb219f2500c8b5cf4e0e555d0d3a27d4
SolijonovAsadbek <iamsolijonovasadbek@gmail.com>
2024-02-16 01:24:45 +0500

    16-fev 01:24

--- handlers/users/about_us.py
+++ handlers/users/about_us.py
@@ -4,6 +4,6 @@ from aiogram import types
 
 @dp.message_handler(text=["Do'konimiz haqida"])
 async def about_us(message: types.Message):
-    telegram_msg = """Bizning do'kon Malika S56 do'kon bo'lib.\n\nBiz bu do'konda <b>Iphone</b> va <b>MacBook</b> sotamiz! Murojat uchun tel raqam: +998911779116
-    """
-    await message.answer(telegram_msg)
+    url = 'media/docs/nasiya.docx'
+    with open(url, 'rb') as doc:
+        await message.answer_document(document=doc, caption="""Nasiya savdo shartlari\n\n<b>sana: 02/05/2024</b>\n\nBog'lanish uchun: \n💬Chat: @macshop_admin\n📞Tel: +998 (91) 797 91 13 \n<a href="https://t.me/macshop_uz">Telegram</a> | <a href="https://www.instagram.com/iseller.uz">Instagram</a>""")
